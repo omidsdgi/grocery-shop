1a5d9379d8a9613e13d875dfc92a3c9c67d412a9
omidsdgi <omid69sdgi@gmail.com>
2025-10-25 09:12:04 +0330

    feat: implement Menu Item

--- app/_components/layouts/header/Header.tsx
+++ app/_components/layouts/header/Header.tsx
@@ -2,7 +2,8 @@ import Link from "next/link";
 import Logo from "@/app/_components/common/ui/logo/Logo";
 import SearchForm from "@/app/_components/layouts/header/search-form/SearchForm";
 import HeaderIcons, {HeaderIconsResponsive} from "@/app/_components/layouts/header/header-icons/HeaderIcons";
-import Menu from "@/app/_components/layouts/header/menu/Menu";
+import MenuList from "@/app/_components/layouts/header/menu/MenuList";
+import { Menu } from 'lucide-react';
 
 export default function Header() {
     return (
@@ -12,22 +13,19 @@ export default function Header() {
                 <SearchForm/>
                 <HeaderIcons/>
                 <button id="menu_btn" className="flex flex-col justify-between py-[4px] lg:hidden w-[24px] h-[24px]">
-                    <span className="w-full h-[1.5px] bg-black inline-block rounded"></span>
-                    <span className="w-full h-[1.5px] bg-black inline-block rounded"></span>
-                    <span className="w-full h-[1.5px] bg-black inline-block rounded"></span>
+                    <Menu size={24} />
                 </button>
             </div>
             <div className="border-gray-200 border-y h">
                 <div
                     className="container transition-all w-4/5 rounded-[24px] lg:rounded-[0px] lg:w-auto flex absolute top-0 bottom-0 -left-[100%] lg:static flex-col lg:flex-row justify-start lg:justify-between items-start pt-[16px] pl-[24px] lg:py-[13px] lg:items-center h-[100vh] bg-white lg:h-[70px] mobile-menu z-50">
-               <Menu/>
-
+                    <MenuList/>
                     <div className="hidden lg:flex items-center shrink-0 gap-3">
-                       <Link href="#"><i
+                        <Link href="#"><i
                             className="icon-headset text-[30px] xl:text-[32px] 2xl:text-[36px] aspect-square"></i></Link>
                         <div>
-                           <Link href="tel:19008888"
-                               className="text-green-200 lg:text-heading6 xl:text-heading5 2xl:text-heading4">1900-8888</Link>
+                            <Link href="tel:19008888"
+                                  className="text-green-200 lg:text-heading6 xl:text-heading5 2xl:text-heading4">1900-8888</Link>
                             <div className="font-lato text-xsmall"><span className="hidden xl:inline-block">24/7 </span>Support
                                 Center
                             </div>
---
+++ app/_components/mock/MenuMock.ts
@@ -0,0 +1,21 @@
+import { Flame } from 'lucide-react';
+export const MenuMock =[
+    {
+        title:"Hot Deals",
+        icon:Flame,
+        link:"#",
+    },
+    {
+        title:"Home",
+        icon:"",
+        link:"#",
+    },  {
+        title:"Food",
+        icon:"",
+        link:"#",
+    },  {
+        title:"Vegetable",
+        icon:"",
+        link:"#",
+    },
+]
