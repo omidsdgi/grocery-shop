6e5be232cd950d47d1e0262f4099c5702044ef06
omidsdgi <omid69sdgi@gmail.com>
2025-10-27 08:14:25 +0330

    feat: install and configure local font Lato in next/font/local

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,16 +6,35 @@ import Header from "@/app/_components/layouts/header/Header";
 import Navigation from "@/app/_components/layouts/navigation/Navigation";
 import Footer from "@/app/_components/layouts/footer/Footer";
 import {Quicksand} from "next/font/google";
+import localfont from "next/font/local"
 
 const quicksand=Quicksand({
     subsets:['latin'],
     display:"swap"
 })
+const lato=localfont({
+    src:[
+        {
+          path:'../public/fonts/Lato-Thin.woff2',
+          weight:'100',
+          style:'normal'
+        },
+        {
+            path:'../public/fonts/Lato-Light.woff2',
+            weight:'300',
+            style:'normal'
+        },
+        {
+            path:'../public/fonts/Lato-Regular.woff2',
+            weight:'400',
+            style:'normal'
+        }
+        ]
 
-
+})
 export const metadata:Metadata = {
     title: {
-       template:'%s/ Grocery Shop',
+        template:'%s/ Grocery Shop',
         default:'Welcome to Grocery Shop',
     },
     description: ' your online destination for fresh groceries, daily essentials, and household products. Shop now for fast delivery and unbeatable quality!',
@@ -26,13 +45,13 @@ interface Props {
 export default function RootLayout({children}: Props) {
     return (
         <html lang="en">
-        <body className={quicksand.className}  >
+        <body className={`${quicksand.className},${lato.className}`}  >
         <header>
-          <Header/>
+            <Header/>
             <Navigation/>
         </header>
         <main> {children}</main>
-       <Footer/>
+        <Footer/>
         </body>
         </html>
     )
