72d96c1795583d9057ac32e700bc31d326d30994
omidsdgi <omid69sdgi@gmail.com>
2025-11-03 23:37:07 +0330

    fix(page): update text color for better readability

--- app/page.tsx
+++ app/page.tsx
@@ -57,7 +57,7 @@ export default function Page() {
               <div className="flex gap-[24px]">
                   <div
                       className="bg-[url('/images/bg-leaf.png')] bg-no-repeat bg-bottom bg-[#3BB77E] rounded-[10px] shadow-[20px_20px_40px_0_rgba(24,24,24,0.07)] p-12 pt-[38px] self-stretch flex-col justify-between max-w-[370px]  xl:flex">
-                      <h3 className="text-heading2 text-blue-300">Bring nature into your home</h3>
+                      <h3 className="text-heading2 text-blue-950">Bring nature into your home</h3>
                       <a href="#"
                          className="mt-6 pl-[15px] pr-2.5 py-2 bg-yellow-100 hover:bg-green-200 rounded-[3px] cursor-pointer inline-flex max-w-max items-center gap-2.5">
                           <div className="text-xsmall text-white">Shop now</div>
