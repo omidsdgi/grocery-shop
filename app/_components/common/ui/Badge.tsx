2fe55d300ad5cb1eb3c5f67482f670ee12548923
omidsdgi <omid69sdgi@gmail.com>
2025-11-04 21:57:30 +0330

    feat(Badge): design component using shadcn/ui

--- app/_components/common/ui/Badge.tsx
+++ app/_components/common/ui/Badge.tsx
@@ -1,22 +1,24 @@
+import { Badge as ShadcnBadge } from "@/components/ui/badge";
 interface Badge {
     badge: string;
     price: number;
     sale_price: number;
 }
-export default function Badge({badge='',sale_price,price}:Badge){
+export default function Badge({badge= "",sale_price,price}:Badge){
 return (
     <>
-        {
-            badge.length > 0 ? (
-                <div
-                    className={`badge ${badge.toLowerCase() == 'hot' ? 'badge--hot' : 'badge--sale'} absolute left-0 top-0 lg:top-5 rounded-tl-[10px] lg:rounded-tl-none`}>{badge}
-                </div>
+        {badge.length > 0 ? (
+                <ShadcnBadge
+                    className={`badge ${badge.toLowerCase() == "hot" ? "badge--hot" : "badge--sale"
+                    } absolute left-0 top-0 lg:top-5 rounded-tl-[10px] lg:rounded-tl-none`}
+                >
+                    {badge}
+                </ShadcnBadge>
             ) : (
-                <div
-                    className="badge badge--off absolute left-0 top-0 lg:top-5 rounded-tl-[10px] lg:rounded-tl-none">-{Math.round((( sale_price / price) * 100))}%</div>
+                <ShadcnBadge className="badge badge--off absolute left-0 top-0 lg:top-5 rounded-tl-[10px] lg:rounded-tl-none">-{Math.round((sale_price / price) * 100)}%
+                </ShadcnBadge>
             )
         }
     </>
 );
 }
-
