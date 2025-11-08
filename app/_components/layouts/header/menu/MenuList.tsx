9ae7184267d14a147ac21bc094101665ecc6a933
omidsdgi <omid69sdgi@gmail.com>
2025-11-08 19:48:54 +0330

    feat: move getMenu call to server component and convert MenuList to client component

--- app/_components/layouts/header/menu/MenuList.tsx
+++ app/_components/layouts/header/menu/MenuList.tsx
@@ -1,13 +1,10 @@
 import CategoriesIcon from "@/app/_components/layouts/header/menu/categories-icon/CategoriesIcon";
 import {MoreButton} from "@/app/_components/layouts/header/menu/categories-icon/MoreButton";
 
-import Link from "next/link";
-import {getMenu} from "@/app/_lib/data-service";
-import {Flame} from "lucide-react";
+import MainMenu from "@/app/_components/layouts/header/menu/main-menu/mainMenu";
 
-export default async function MenuList() {
-const {mainMenu}= await getMenu();
-    return (
+export default  function MenuList() {
+      return (
         <>
             <div id="all_categories"
                  className="flex relative cursor-pointer bg-green-200 gap-2.5 text-white px-4 py-3 rounded-[5px] items-center">
@@ -15,31 +12,15 @@ const {mainMenu}= await getMenu();
                 <div className="text-medium">Browse All Categories</div>
                 <i className="icon-angle-small-down text-[24px]"></i>
                 <div id="all_categories_box"
-                     className="hidden absolute z-20 bg-white left-0 top-16 w-[500px] rounded-[5px] border-[1px] border-green-300 p-[30px] hover:cursor-default">
+                     className=" absolute z-20 bg-white left-0 top-16 w-[500px] rounded-[5px] border-[1px] border-green-300 p-[30px] hover:cursor-default">
                     <div id="all_cat_inner_box" className="flex flex-wrap justify-between gap-y-[15px]">
                         <CategoriesIcon/>
-                        <MoreButton text={"More Categories"}/>
+                        <MoreButton text={"Show More Category"}/>
                     </div>
                 </div>
             </div>
-            <nav id="main_menu">
-                <ul className="flex flex-col lg:flex-row items-start lg:items-center text-heading6 lg:text-heading-sm 2xl:text-heading6 gap-[32px] mt-[32px] lg:mt-0 lg:gap-3 xl:gap-5 2xl:gap-10">
-
-                    {mainMenu.map((item,index) =>{
-                        return (
-                            <li key={index}>{
-                                item.icon_name ?
-                                    (<Link href="#"  className="flex flex-row gap-2 items-center">
-                                            <Flame className="text-[24px]"/>
-                                            <div className="text-heading6 lg:text-heading-sm xl:text-heading6">{item.title}</div>
-                                        </Link>
-                                    ) :(
-                                        <Link href="#" className="flex items-center gap-1">{item.title}</Link>)
-                            }
-                            </li>)})}
-                </ul>
-            </nav>
-        </>
+            <MainMenu/>
+              </>
     )
 }
 
