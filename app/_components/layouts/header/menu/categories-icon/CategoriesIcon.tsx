ee814f7949d6846260f49411ed60550488c4ca2f
omidsdgi <omid69sdgi@gmail.com>
2025-11-08 11:53:54 +0330

    feat: replace browseCategoryMock with real data fetched via getMenu()

--- app/_components/layouts/header/menu/categories-icon/CategoriesIcon.tsx
+++ app/_components/layouts/header/menu/categories-icon/CategoriesIcon.tsx
@@ -1,17 +1,19 @@
 import {CategoryCard} from "@/app/_components/layouts/header/menu/categories-icon/CategoryCard";
-import {categoriesMock} from "@/app/_components/mock/catergoriesMock";
+// import {categoriesMock} from "@/app/_components/mock/catergoriesMock";
+import {getMenu} from "@/app/_lib/data-service";
 
-export default function CategoriesIcon() {
+export default async function CategoriesIcon() {
+    const {browseCategory}=await getMenu()
 
     return (
         <div className="flex flex-wrap gap-4">
-            {categoriesMock.map((category,index) => (
+            {browseCategory.map((category,index) => (
                 <CategoryCard
                     key={index}
-                    icon={category.icon}
-                    pathCount={category.pathCount}
+                    icon={category.icon_name}
+                    pathCount={category.icon_path}
                     title={category.title}
-                    href={category.href}
+                    href={category.link}
                 />
             ))}
         </div>
