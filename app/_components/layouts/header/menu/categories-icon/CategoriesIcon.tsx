594ec609cba33f2c222a0bd425f022f775ffe37d
omidsdgi <omid69sdgi@gmail.com>
2025-11-08 23:38:39 +0330

    refactor(CategoryIcon): add CategoryIconProps

--- app/_components/layouts/header/menu/categories-icon/CategoriesIcon.tsx
+++ app/_components/layouts/header/menu/categories-icon/CategoriesIcon.tsx
@@ -1,15 +1,13 @@
 import {CategoryCard} from "@/app/_components/layouts/header/menu/categories-icon/CategoryCard";
-// import {categoriesMock} from "@/app/_components/mock/catergoriesMock";
-import {getMenu} from "@/app/_lib/data-service";
+import {CategoriesIconProps} from "@/components/type/MenuItemType";
 
-export default async function CategoriesIcon() {
-    const {browseCategory}=await getMenu()
+export default  function CategoriesIcon({browseCategory}:CategoriesIconProps) {
 
     return (
         <div className="flex flex-wrap gap-4">
-            {browseCategory.map((category,index) => (
+            {browseCategory?.map((category) => (
                 <CategoryCard
-                    key={index}
+                    key={category.id}
                     icon={category.icon_name}
                     pathCount={category.icon_path}
                     title={category.title}
