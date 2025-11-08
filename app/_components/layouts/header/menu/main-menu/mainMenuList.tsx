509479aa6fac32085e9008ded184d9d59f5b10ec
omidsdgi <omid69sdgi@gmail.com>
2025-11-08 23:36:07 +0330

    refactor(mainMenuList): add MainMenuListProps and revise code

---
+++ app/_components/layouts/header/menu/main-menu/mainMenuList.tsx
@@ -0,0 +1,35 @@
+import Link from "next/link";
+import {Flame} from "lucide-react";
+import {MainMenuListProps} from "@/components/type/MenuItemType";
+
+export default async function MainMenuList({mainMenu}:MainMenuListProps) {
+    return (
+        <nav id="main_menu">
+            <ul className="flex flex-col lg:flex-row items-start lg:items-center text-heading6 lg:text-heading-sm 2xl:text-heading6 gap-[32px] mt-[32px] lg:mt-0 lg:gap-3 xl:gap-5 2xl:gap-10">
+                {mainMenu?.map((item) => (
+                    <li key={item.id}>
+                        {item.icon_name ? (
+                            <Link
+                                href={item.link}
+                                className="flex flex-row gap-2 items-center"
+                            >
+                                <Flame className="text-[24px]"/>
+                                <div className="text-heading6 lg:text-heading-sm xl:text-heading6">
+                                    {item.title}
+                                </div>
+                            </Link>
+                        ) : (
+                            <Link
+                                href={item.link}
+                                className="flex items-center gap-1"
+                            >
+                                {item.title}
+                            </Link>
+                        )}
+                    </li>
+                ))}
+            </ul>
+        </nav>
+    );
+}
+
