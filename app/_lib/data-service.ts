129a2cff77b86eb7861b13ff9162fbec61491ee3
omidsdgi <omid69sdgi@gmail.com>
2025-11-14 22:15:20 +0330

    feat: add Supabase query for FeaturedCategories

--- app/_lib/data-service.ts
+++ app/_lib/data-service.ts
@@ -1,5 +1,6 @@
 import {supabase} from "./Supabase"
 import {MenuGroups} from "@/components/type/MenuItemType";
+import {FeaturedCategoryTpe} from "@/components/type/FeaturedCategoryType";
 
 export async function getMenu():Promise<MenuGroups> {
     const {data,error}=await supabase
@@ -13,5 +14,29 @@ export async function getMenu():Promise<MenuGroups> {
     const mainMenu = data.filter(item => item.menuId === 1);
     const browseCategory = data.filter(item => item.menuId ===2);
     return {mainMenu,browseCategory};
+}
+
+export async function getFeaturedCategory() :Promise<FeaturedCategoryTpe[]> {
+    const {data, error}=await supabase
+        .from("featured-category")
+        .select(`
+        id,
+        created_at,
+        title,
+        link,
+        color,
+        image,
+        items,
+        is_featured
+        `)
+         .eq("is_featured",false);
+
+    if (error){
+        console.error('Error fetching featured category:',error);
+        throw new Error('Failed to fetch featured category');
+    }
+
+    console.log("featured_category:",data)
+    return data ||[];
 }
 
