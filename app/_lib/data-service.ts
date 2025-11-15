6a4a7a7e48d879c40fd2921afd2e7afa23256145
omidsdgi <omid69sdgi@gmail.com>
2025-11-15 23:13:51 +0330

    feat: add Supabase query for product categories

--- app/_lib/data-service.ts
+++ app/_lib/data-service.ts
@@ -1,6 +1,7 @@
 import {supabase} from "./Supabase"
 import {MenuGroups} from "@/components/type/MenuItemType";
 import {FeaturedCategoryTpe} from "@/components/type/FeaturedCategoryType";
+import {ProductType} from "@/components/type/ProductType";
 
 export async function getMenu():Promise<MenuGroups> {
     const {data,error}=await supabase
@@ -27,7 +28,20 @@ export async function getFeaturedCategory() :Promise<FeaturedCategoryTpe[]> {
         throw new Error('Failed to fetch featured category');
     }
 
-    console.log("featured_category:",data)
+    return data ||[];
+}
+type ProductFilter = 'is_popular' | 'is_top_selling' | 'is_trending' | 'is_popular_fruit' | 'is_best_seller';
+
+export async function getProductsByFilter(filter:ProductFilter):Promise<ProductType[]>{
+    const {data, error}=await supabase
+        .from('product')
+        .select('*')
+        .eq("filter",true);
+
+    if (error){
+        console.error(`Error fetching products with filter ${filter}:`,error);
+        throw new Error(`Failed to fetch products with filter ${filter}`);
+    }
     return data ||[];
 }
 
