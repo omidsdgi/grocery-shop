0f478e925cdb2c8e7a0e99955e153a48c4f4351d
omidsdgi <omid69sdgi@gmail.com>
2025-11-17 23:24:04 +0330

    fix(Badge): handle undefined sale_price safely in discount badge calculation

--- app/_components/common/ui/Badge.tsx
+++ app/_components/common/ui/Badge.tsx
@@ -2,7 +2,7 @@ import { Badge as ShadcnBadge } from "@/components/ui/badge";
 interface Badge {
     badge: string;
     price: number;
-    sale_price: number;
+    sale_price?: number;
 }
 export default function Badge({badge= "",sale_price,price}:Badge){
 return (
@@ -14,10 +14,10 @@ return (
                 >
                     {badge}
                 </ShadcnBadge>
-            ) : (
+            ) : sale_price ? (
                 <ShadcnBadge className="badge badge--off absolute left-0 top-0 lg:top-5 rounded-tl-[10px] lg:rounded-tl-none">-{Math.round((sale_price / price) * 100)}%
                 </ShadcnBadge>
-            )
+            ) : null
         }
     </>
 );
