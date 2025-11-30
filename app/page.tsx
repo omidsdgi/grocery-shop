69b67258b85a18898cde6e54eb088aed1f9b4d41
omidsdgi <omid69sdgi@gmail.com>
2025-11-30 22:27:28 +0330

    feat(best-seller): switch layout to flex column on md screens

--- app/page.tsx
+++ app/page.tsx
@@ -57,9 +57,9 @@ export default async function Page() {
                   <h2 className="text-heading6 md:text-heading5 lg:text-heading4 xl:text-heading3 text-blue-300">Best
                       Sellers </h2>
               </div>
-              <div className="flex gap-[24px]">
+              <div className="flex flex-col md:flex-row gap-[24px]">
                   <div
-                      className="bg-[url('/images/bg-leaf.png')] bg-no-repeat bg-bottom bg-[#3BB77E] rounded-[10px] shadow-[20px_20px_40px_0_rgba(24,24,24,0.07)] p-12 pt-[38px] self-stretch flex-col justify-between max-w-[370px]  xl:flex">
+                      className="bg-[url('/images/bg-leaf.png')] bg-no-repeat bg-bottom bg-[#3BB77E] rounded-[10px] shadow-[20px_20px_40px_0_rgba(24,24,24,0.07)] p-8 xl:p-12 pt-[38px] flex flex-col justify-between xl:max-w-[370px] xl:self-stretch">
                       <h3 className="text-heading2 text-blue-950">Bring nature into your home</h3>
                       <a href="#"
                          className="mt-6 pl-[15px] pr-2.5 py-2 bg-yellow-100 hover:bg-green-200 rounded-[3px] cursor-pointer inline-flex max-w-max items-center gap-2.5">
@@ -67,12 +67,12 @@ export default async function Page() {
                           <i className="icon-arrow-small-right text-[24px]"></i>
                       </a>
                   </div>
-                     <ProductSection
-                         products={bestSeller}
-                         showNavigation={false}
-                         slidesPerView={3}
-                         />
-                  </div>
+                  <ProductSection
+                      products={bestSeller}
+                      showNavigation={false}
+                      slidesPerView={3}
+                  />
+              </div>
           </Section>
           <Section>
               <div className="flex justify-between items-center mb-[50px]">
