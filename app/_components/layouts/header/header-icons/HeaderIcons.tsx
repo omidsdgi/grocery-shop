cd43f0bcc38fd3d11c0e309bc8ece134119f63db
omidsdgi <omid69sdgi@gmail.com>
2025-12-09 19:34:46 +0330

    feat: show user avatar in navbar when session image exists, fallback to default icon otherwise

--- app/_components/layouts/header/header-icons/HeaderIcons.tsx
+++ app/_components/layouts/header/header-icons/HeaderIcons.tsx
@@ -1,14 +1,29 @@
 import { User, ShoppingCart } from "lucide-react";
 import DarkModeToggle from "@/app/_components/common/ui/DarkModeToggle";
+import {auth} from "@/app/_lib/auth";
+import Image from "next/image";
 
-export default function HeaderIcons() {
+export default async function HeaderIcons() {
+    const session=await auth();
     return (
-        <ul className="hidden lg:flex gap-5">
-            <li className="flex gap-2 cursor-pointer items-center">
-                <a href="#">
-                    <User className="h-6 w-6"/>
+        <ul className="hidden lg:flex  gap-5">
+            <li className="flex gap-2 cursor-pointer items-center justify-center">
+                <a href="#" className="flex items-center gap-2">
+                    {session?.user?.image ? (
+                        <Image
+                            className="h-6 w-6 rounded-full object-cover"
+                            src={session.user.image}
+                            alt={session.user.name || "User avatar"}
+                            height={24}
+                            width={24}
+                            referrerPolicy="no-referrer"
+                        />
+                    ) : (
+                        <User className="h-6 w-6"/>
+                    )
+                    }
+                    <div className="hidden xl:inline-block text-sm text-gray-500 font-lato">Account</div>
                 </a>
-                <div className="hidden xl:inline-block text-sm text-gray-500 font-lato">Account</div>
             </li>
             <li>
                 <DarkModeToggle/>
