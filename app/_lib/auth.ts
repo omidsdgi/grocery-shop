657e82c5339fac66c51e1259ab41ad1390cffb1a
omidsdgi <omid69sdgi@gmail.com>
2025-12-10 17:22:42 +0330

    feat: add Google authentication provider with authorization callback

--- app/_lib/auth.ts
+++ app/_lib/auth.ts
@@ -7,7 +7,12 @@ const authConfig = {
             clientId:process.env.AUTH_GOOGLE_ID,
             clientSecret:process.env.AUTH_GOOGLE_SECRET,
         })
-    ]
+    ],
+    callbacks:{
+        authorized({auth}:{auth: any})  {
+            return !!auth?.user;
+        }
+    }
 }
 
 export const {auth, handlers: {GET, POST}} = NextAuth (authConfig)
