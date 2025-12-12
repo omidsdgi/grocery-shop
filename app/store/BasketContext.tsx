ef786d6d6a284e97369bc138baff05cc6b481465
omidsdgi <omid69sdgi@gmail.com>
2025-12-12 20:49:14 +0330

    feat: implement basket item management handlers

--- app/store/BasketContext.tsx
+++ app/store/BasketContext.tsx
@@ -29,13 +29,49 @@ export const BasketContext = createContext<{
 export const BasketContextProvider = ({ children }: Props) => {
     const [basketItems, setBasketItems] = useState<Array<ProductItem>>([]);
 
-    const addItemHandler = () => {};
+    const addItemHandler = (product: ProductItem) => {
+        const newProduct: ProductItem = {
+            productId: product.productId,
+            title: product.title,
+            img: product.img,
+            price: product.price,
+            quantity: 1,
+        };
+        setBasketItems((prevState) => [...prevState, newProduct]);
+    };
 
-    const increaseItemHandler = () => {};
+    const increaseItemHandler = (productId: number) => {
+        const newBasket = basketItems.map((item) =>
+            item.productId === productId
+                ? { ...item, quantity: item.quantity + 1 }
+                : item
+        );
+        setBasketItems(newBasket);
+    };
 
-    const decreaseItemHandler = () => {};
+    const decreaseItemHandler = (productId: number) => {
+        const currentProduct = basketItems.find(
+            (item) => item.productId === productId
+        );
 
-    const deleteItemHandler = () => {};
+        if (currentProduct && currentProduct.quantity === 1) {
+            deleteItemHandler(productId);
+        } else {
+            const newBasket = basketItems.map((item) =>
+                item.productId === productId
+                    ? { ...item, quantity: item.quantity - 1 }
+                    : item
+            );
+            setBasketItems(newBasket);
+        }
+    };
+
+    const deleteItemHandler = (productId: number) => {
+        const newBasket = basketItems.filter(
+            (item) => item.productId !== productId
+        );
+        setBasketItems(newBasket);
+    };
 
     return (
         <BasketContext.Provider
